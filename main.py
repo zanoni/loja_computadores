889d4cd892be53e2fdf4d3d2bc99526a833236d9
zanoni <zanoni.reibnitz@gmail.com>
2019-10-17 17:18:37 -0300

    ew

--- main.py
+++ main.py
@@ -1,5 +1,20 @@
 from flask import Flask, render_template
 
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/compra')
+def compra():
+    return render_template('compra.html')
+
+@app.route('/lista')
+def lista():
+    return render_template('lista.html')
+
+app.run()
 
 
 
