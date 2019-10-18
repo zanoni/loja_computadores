27697e2fc63930bf88076331cd676b06492d9ef3
Leonardo José da Silva <leonsilva700@gmail.com>
2019-10-18 11:35:13 -0300

    loja

--- testconexao.py
+++ testconexao.py
@@ -1,14 +1,79 @@
 from flask import Flask, render_template, request, redirect
+from produto import Produto
 import MySQLdb
 
 ##produto
+app = Flask(__name__)
 
+
+
+
+
+
+img_produto = []
 def imagem_produto_db():
     conexao = MySQLdb.connect(host="mysql.zuplae.com", user="zuplae11", passwd="grupo06", database="zuplae11")
     cursor = conexao.cursor()
-    img_produto = cursor.execute("SELECT Imagem FROM zuplae11.Produto WHERE ID=1;")    
+    cursor.execute('SELECT * FROM Produto') 
+    img = []
+    for i in cursor.fetchall():
+        produto = Produto()
+        produto.img = i[4]
+        img.append(produto.img)
     conexao.close()
     return img_produto
+
+
+@app.route('/form/test')
+def imagem_test():
+    img_test = imagem_produto_db()
+    return render_template('teste.html',img_test = img_test)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+app.run()
+
+
+
+
 
 
 
